17a92c31bd1be349ca0793944d8de64d70e49ce3
Keisuke Yamazaki <ma17123@shibaura-it.ac.jp>
2019-11-17 17:48:18 +0900

    Initial Commit

---
+++ File Syncer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  File Syncer
+//
+//  Created by Yamazaki Keisuke on 2019/11/17.
+//  Copyright © 2019 Yamazaki Keisuke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
